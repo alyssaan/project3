d78251e860b0975321941612395612fb1ee64652
alyssaan <93748829+alyssaan@users.noreply.github.com>
2023-03-06 23:59:45 -0600

    Update madlibs.js

--- madlibs.js
+++ madlibs.js
@@ -2,5 +2,5 @@ const madlibs = ['I was {{-ing verb}} in {{course}} when all of a sudden I looke
 	'I {{-ed verb}} around campus today. I decided to enter {{place}} to do homework. Suddenly, {{person}} appeared and took my homework! I tried to tell my professor, but they became {{feeling}}. Oh well...',
 	'Today in {{course}} there was a surprise lecture by {{person}}. In the middle, they suddenly began {{-ing verb}}. Everyone felt really {{feeling}}!',
 	'I was feeling {{feeling}} for my {{course}} test today. On my way though, I saw {{person}} {{-ing verb}} around campus. After that I felt less nervous!'
-	'One day, after my roommate {{-ed verb}} in our {{adj}} room, I decided I wanted to live at {{places}} instead. I'm {{feeling}} about my new living situation!']
+	'One day, after my roommate {{-ed verb}} in our {{adj}} room, I decided I wanted to live at {{places}} instead. I\'m {{feeling}} about my new living situation!']
   
