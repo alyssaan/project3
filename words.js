38686c6fb2ef51d401c72856eff0c86daed69667
alyssaan <93748829+alyssaan@users.noreply.github.com>
2023-03-06 23:30:31 -0600

    created words.js

---
+++ words.js
@@ -0,0 +1,17 @@
+const ingVerbs = ['walking', 'dancing', 'cooking', 'crying', 'shopping', 'laughing', 'crochet', 'snorkeling', 'hula-hooping', 'cleaning', 'cheering', 'curling', 'bobsledding', 'skipping']
+
+const courses = ['Media Aesthetics', 'Internet Art', 'Self, Culture, and Society', 'General Chemistry', 'Principles of Microeconomics', 'Philosophical Perspectives']
+
+const adjectives = ['yellow', 'dry', 'old', 'dirty', 'clear', 'green', 'metal', 'small', 'ambitious', 'oily']
+
+const nouns = ['shoes', 'octopi', 'cans', 'phones', 'chocolates', 'pencils', 'shirts', 'chopsticks', 'napkins', 'plates']
+
+const edVerbs = ['walked', 'danced', 'cooked', 'cried', 'shopped', 'laughed', 'crocheted', 'snorkeled', 'hula-hooped', 'cleaned', 'cheered', 'curled', 'bobsled', 'skipped']
+  
+const feelings = ['happy', 'sad', 'excited', 'scared', 'worried', 'dazed', 'enraged', 'depresed', 'scared', 'overjoyed']
+
+const people = ['Dean Boyer', 'Professor Briz', 'Dean Nandorf', 'President Alivisatos', 'Professor Satrom', 'Professor Jagoda']
+ 
+const places = ['the Reg', 'Harper', 'Woodlawn', 'Crerar', 'Plein Air', 'Mansueto', 'Rockefeller Chapel']
+    
+
